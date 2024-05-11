cedf0f6b00a59e0ff4522ccba4e1ae6b3e6923d2
hwangYunha <onlyunha@dankook.ac.kr>
2024-05-12 02:56:07 +0900

    edit2

--- lab2/queue/queue_impl.cpp
+++ lab2/queue/queue_impl.cpp
@@ -1,59 +1,53 @@
 #include "queue.h"
 #include "queue_impl.h"
-#include <mutex>
-#include <condition_variable>
 
 
 void Queue::enqueue (int key, int value) {
-    std::lock_guard<std::mutex> lock(mutex_);
     data_.push_back({key, value});
 }
 
 std::pair<int, int> Queue::dequeue () {
-	std::lock_guard<std::mutex> lock(mutex_);
-	if (data_.empty())
-		return {-1, -1};
-	auto front = data_.front();
-	data_.erase(data_.begin());
-    return front;
-}
-
-bool Queue::empty () {
-    std::lock_guard<std::mutex> lock(mutex_);
-    return data_.empty();
-}
-
-void CoarseQueue::enqueue (int key, int value) {
-	std::lock_guard<std::mutex> lock(mutex_);
-    data_.push_back({key, value});
-}
-
-std::pair<int, int> CoarseQueue::dequeue () {
-	std::lock_guard<std::mutex> lock(mutex_);
-	if (data_.empty())
-		return {-1, -1};
+    if (data_.empty()) {
+        return {-1, -1};
+    }
     auto front = data_.front();
     data_.erase(data_.begin());
     return front;
 }
 
-bool CoarseQueue::empty () {
-    std::lock_guard<std::mutex> lock(mutex_);
+bool Queue::empty () {
     return data_.empty();
+}
+
+void CoarseQueue::enqueue (int key, int value) {
+	pthread_mutex_lock(&mutex_lock);
+    data_.push_back({key, value});
+    pthread_mutex_unlock(&mutex_lock);
+}
+
+std::pair<int, int> CoarseQueue::dequeue () {
+	pthread_mutex_lock(&mutex_lock);
+	if (data_.empty()) {
+		return {-1, -1};
+    }
+    auto front = data_.front();
+    data_.erase(data_.begin());
+    pthread_mutex_unlock(&mutex_lock);
+    return front;
+}
+
+bool CoarseQueue::empty () {
+    pthread_mutex_lock(&mutex_lock);
+    bool isEmpty = data_.empty();
+    pthread_mutex_unlock(&mutex_lock);
+    return isEmpty;
 }
 
 void FineQueue::enqueue (int key, int value) {
     pthread_mutex_lock(&mutex_lock);
-    while (size_ >= capacity_) {
-        int new_capacity = capacity_ * 2;
-        std::vector<std::pair<int, int>> new_data(new_capacity);
-        for (int i = 0; i < size_; ++i) {
-            new_data[i] = data_[(front_ + i) % capacity_];
-        }
-        data_ = std::move(new_data);
-        capacity_ = new_capacity;
-        front_ = 0;
-        rear_ = size_;
+    if (size_ >= capacity_) {
+        pthread_mutex_unlock(&mutex_lock);
+        return;
     }
     data_[rear_] = {key, value};
     rear_ = (rear_ + 1) % capacity_;
