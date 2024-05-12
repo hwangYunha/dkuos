053db8ae1e6d5c1cdfb55aef6a97a2ea0d6f48f3
hwangYunha <onlyunha@dankook.ac.kr>
2024-05-12 21:47:39 +0900

    cv

--- lab2/queue/queue_impl.cpp
+++ lab2/queue/queue_impl.cpp
@@ -49,24 +49,24 @@ bool CoarseQueue::empty () {
 void FineQueue::enqueue(int key, int value) {
     pthread_mutex_lock(&mutex_lock);
     while (size_ >= capacity_) {
-        pthread_cond_wait(&en_cv_, &mutex_lock);
+        pthread_cond_wait(&cv_, &mutex_lock);
     }
     data_[rear_] = {key, value};
     rear_ = (rear_ + 1) % capacity_;
     ++size_;
-    pthread_cond_signal(&de_cv_);
+    pthread_cond_signal(&cv_);
     pthread_mutex_unlock(&mutex_lock);
 }
 
 std::pair<int, int> FineQueue::dequeue() {
     pthread_mutex_lock(&mutex_lock);
     while (size_ <= 0) {
-        pthread_cond_wait(&de_cv_, &mutex_lock);
+        pthread_cond_wait(&cv_, &mutex_lock);
     }
     auto item = data_[front_];
     front_ = (front_ + 1) % capacity_;
     --size_;
-    pthread_cond_signal(&en_cv_);
+    pthread_cond_signal(&cv_);
     pthread_mutex_unlock(&mutex_lock);
     return item;
 }
